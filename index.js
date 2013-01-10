b843e53465f0748a731286f92e3ea7c3ba9e2729
George Stagas <gstagas@gmail.com>
2013-01-11 01:57:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+var domify = require('domify')
+var css = require('css')
+var rect = require('rect')
+var v = require('vector')
+
+var domEl = function (el) {
+  if (el) return el
+  else return domify('<div class="entity entity-dom"></div>')[0]
+}
+
+module.exports = function (parentEl) {
+  var dom = {}
+  
+  dom.el = [domEl]
+
+  dom.render = function (e) {
+    var pos = v(e.mesh.pos).sub(e.offset)
+    css(e.el, {
+      left: Math.round(pos.left)
+    , top: Math.round(pos.top)
+    })
+  }
+
+  dom.init = function (e) {
+    if (e.id) e.el.attributes.id = e.id
+    if (e.class) e.el.classList.add(e.class)
+    var size = e.mesh.size
+    css(e.el, {
+      width: size.width
+    , height: size.height
+    })
+  }
+
+  dom.start = function (e) {
+    parentEl.appendChild(e.el)
+  }
+
+  dom.stop = function (e) {
+    parentEl.removeChild(e.el)
+  }
+
+  return dom
+}
